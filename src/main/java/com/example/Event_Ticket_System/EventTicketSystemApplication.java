a7e3f402ba06073b4d498b4f82366381296ae305
toantran980 <jokemaking90@gmail.com>
2026-03-04 15:23:04 -0800

    initial commit

---
+++ src/main/java/com/example/Event_Ticket_System/EventTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Event_Ticket_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTicketSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Event_Ticket_System/EventTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Event_Ticket_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
