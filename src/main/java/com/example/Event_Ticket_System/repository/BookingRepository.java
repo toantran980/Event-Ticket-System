c829adb220587d7fbdd8eb07c9308b39cc3b2762
Mewbs <mosesbui0921@gmail.com>
2026-03-14 18:26:45 -0700

    pushing booking repo

--- src/main/java/com/example/Event_Ticket_System/repository/BookingRepository.java
+++ src/main/java/com/example/Event_Ticket_System/repository/BookingRepository.java
@@ -1,6 +1,8 @@
 package com.example.Event_Ticket_System.repository;
 
 import com.example.Event_Ticket_System.entity.Booking;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,4 +19,35 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
            "WHERE b.ticketType.event.event_id = :eventId " +
            "AND b.payment_status = 'CONFIRMED'")
     List<Booking> findConfirmedBookingsByEventId(@Param("eventId") Integer eventId);
+
+    //Calculate total revenue for an event
+    @Query("SELECT SUM(b.ticketType.price) FROM Booking b " +
+            "WHERE b.ticketType.event.event_id = :eventId " +
+            "AND b.payment_status = 'CONFIRMED'")
+    Double calculateTotalRevenueByEventID(@Param("eventID") Integer eventID);
+
+    // Get all booking for a specific attendee
+    List<Booking> findByAttendeeAttendeeId(Integer attendeeId);
+
+    //Get all bookings for a specific ticket type
+    List<Booking> findByTicketTypeTicketTypeId(Integer ticketTypeId);
+
+    // Counts confirmed bookings for an event
+    @Query("SELECT COUNT(b) FROM Booking b " +
+            "WHERE b.ticketType.event.event_id = :eventId " +
+            "AND b.payment_status = 'CONFIRMED'")
+    Long countConfirmedBookingsByEventID(@Param("eventId") Integer eventId);
+
+    // Gets bookings for an event with a specific status
+    @Query("SELECT b FROM Booking b " +
+    "WHERE b.ticketType.event.event_id = :eventId " +
+    "AND b.payment_status = :status")
+    List<Booking> findBookingsByEventIdAndStatus(@Param("eventId") Integer eventId, @Param("status") String status);
+
+    // Paginated confirmed bookings for an event
+    @Query("SELECT b FROM Booking b " +
+    "WHERE b.ticketType.event.event_id = :eventId " +
+    "AND b.payment_status = 'CONFIRMED'")
+    Page<Booking> findConfirmedBookingsByEventId(@Param("eventId") Integer eventId, Pageable pageable);
+
 }
